2f1255b6257b5ec1c1c8084806a9eed9ba902d5a
rumentoshev <monkey23toshev@abv.bg>
2022-01-18 22:14:39 +0200

    Login function added

--- ProektMMO_Final/ProjectMMO_IPFMI.cpp
+++ ProektMMO_Final/ProjectMMO_IPFMI.cpp
@@ -54,3 +54,60 @@ void Home_Menu(fstream& Users)
 		Home_Menu(Users);
 	}
 }
+void Login(fstream& Users)
+{
+	string username;
+	string password;
+	string buffer;
+	string buffer_username;
+	string buffer_password;
+	string buffer_level;
+	string hashed_password;
+	hash<string> hash_obj;
+	int level;
+	bool flag = false;
+	cout << "Plese enter your username:\n";
+	cin >> username;
+	cout << "Plese enter your password:\n";
+	cin >> password;
+	hashed_password = to_string(hash_obj(password));
+	Users.open("users.txt", fstream::in);
+	if (Users.is_open())
+	{
+		while (getline(Users, buffer))
+		{
+			int i = 0;
+			while (i < buffer.size() && buffer[i] != ':')
+			{
+				buffer_username += buffer[i++];
+			}
+			i++;
+			while (i < buffer.size() && buffer[i] != ':')
+			{
+				buffer_password += buffer[i++];
+			}
+			i++;
+			while (i < buffer.size())
+			{
+				buffer_level += buffer[i++];
+			}
+			level = stoi(buffer_level);
+			if ((username == buffer_username) && (hashed_password == buffer_password))
+			{
+				Users.close();
+				flag = true;
+				Menu2(Users, username, hashed_password, level);
+				break;
+			}
+			buffer_username = "";
+			buffer_password = "";
+			buffer_level = "";
+		}
+		if (flag == false)
+		{
+			Users.close();
+			cout << "Invalid information!Plese try again!" << endl;
+			Login(Users);
+		}
+	}
+}
