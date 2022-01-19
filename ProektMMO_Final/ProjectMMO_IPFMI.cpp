e1ee4d6240e4135096b00f3e99e5b6fbb4c92e67
rumentoshev <monkey23toshev@abv.bg>
2022-01-19 19:39:56 +0200

    Registration function added

--- ProektMMO_Final/ProjectMMO_IPFMI.cpp
+++ ProektMMO_Final/ProjectMMO_IPFMI.cpp
@@ -22,6 +22,7 @@ using namespace std;
 void Home_Menu(fstream& Users);
 void Login(fstream& Users);
 void Registration(fstream& Users);
+void Registration(fstream& Users)
 
 int main()
 {
@@ -109,5 +110,128 @@ void Login(fstream& Users)
 			cout << "Invalid information!Plese try again!" << endl;
 			Login(Users);
 		}
+	}
+}
+void Registration(fstream& Users)
+{
+	int level = 1;
+	string username;
+	string password;
+	string hashed_password;
+	hash<string> hash_obj;
+	int number_counter = 0;
+	int upleter_counter = 0;
+	int lowleter_counter = 0;
+	int symbol_counter = 0;
+	int error_pass_counter = 0;
+	bool isValidPass = false;
+	bool isValidUsername = false;
+	int correct_username_counter = 0;
+	int error_username_counter = 0;
+	cout << "Plese enter username which has only latin letters and numbers:\n";
+	cin >> username;
+	for (int i = 0; i < username.size(); i++)
+	{
+		if (username[i] >= 48 && username[i] <= 57)
+		{
+			correct_username_counter++;
+		}
+		else if (username[i] >= 97 && username[i] <= 122)
+		{
+			correct_username_counter++;
+		}
+		else if (username[i] >= 65 && username[i] <= 90)
+		{
+			correct_username_counter++;
+		}
+		else
+		{
+			error_username_counter++;
+		}
+	}
+	if (correct_username_counter == username.size() && username.size() >= 5)
+	{
+		isValidUsername = true;
+	}
+	else
+	{
+		cout << "Incorect username!\n";
+		Registration(Users);
+	}
+	cout << "Plese enter password using only latin letters,numbers and (!@#$%^&*) symbols :\n";
+	cout << "Be sure you have at least 1 uper letter,1 lower letter, 1 number and 1 symbol! \n";
+	cin >> password;
+
+	for (int i = 0; i < password.size(); i++)
+	{
+		if (password[i] >= 48 && password[i] <= 57)
+		{
+			number_counter++;
+		}
+		else if (password[i] >= 65 && password[i] <= 90)
+		{
+			upleter_counter++;
+		}
+		else if (password[i] >= 97 && password[i] <= 122)
+		{
+			lowleter_counter++;
+		}
+		else if (password[i] == 33 || password[i] == 64 || password[i] == 42 || password[i] == 94)
+		{
+			symbol_counter++;
+		}
+		else if (password[i] >= 35 && password[i] <= 38)
+		{
+			symbol_counter++;
+		}
+		else
+		{
+			error_pass_counter++;
+		}
+	}
+	if (number_counter >= 1 && upleter_counter >= 1 && lowleter_counter >= 1 && symbol_counter >= 1 && password.size() >= 5)
+	{
+		isValidPass = true;
+		hashed_password = to_string(hash_obj(password));
+	}
+	else
+	{
+		cout << "Incorect password!\n";
+		Registration(Users);
+	}
+	bool flag = true;
+	string buffer;
+	string buffer_username;
+	Users.open("users.txt", fstream::in);
+	if (Users.is_open())
+	{
+		while (getline(Users, buffer))
+		{
+			int i = 0;
+			while (i < buffer.size() && buffer[i] != ':')
+			{
+				buffer_username += buffer[i++];
+			}
+			if (username == buffer_username)
+			{
+				flag = false;
+				break;
+			}
+			buffer_username = "";
+		}
+	}
+	Users.close();
+	if (flag == false)
+	{
+		cout << "There is already user with that name!\nPlease try another username!" << endl;
+		Registration(Users);
+	}
+
+	if (isValidPass == true && isValidUsername == true)
+	{
+		Users.open("users.txt", fstream::out | fstream::app);
+		Users << username << ":" << hashed_password << ":" << level << endl;
+		Users.close();
+		Menu2(Users, username, hashed_password, level);
 	}
 }
